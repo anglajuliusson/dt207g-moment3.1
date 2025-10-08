0f7db5953efe79bb4e9981251303e539a9ffd5d8
Ängla Juliusson <angla02@icloud.com>
2025-10-08 09:46:26 +0200

    Anslut till databasen

--- server.js
+++ server.js
@@ -11,6 +11,13 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+// Connect to MongoDB
+mongoose.connect("").then(() => {
+    console.log("Connected to MongoDB");
+}).catch((error) => {
+    console.log("Error connecting to database: " + error)
+})
+
 // Routes
 app.get("/api", async (req, res) => {
     res.json({message: "Welcome to this API"});
