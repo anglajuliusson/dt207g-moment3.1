824f882bf0cbb88e7413956a698794d03519a386
Ängla Juliusson <angla02@icloud.com>
2025-10-08 10:02:46 +0200

    Work Schema med attribut

--- server.js
+++ server.js
@@ -18,6 +18,30 @@ mongoose.connect("mongodb://localhost:27017/work_experiences").then(() => {
     console.log("Error connecting to database: " + error)
 })
 
+// Work Schema
+const workSchema = new mongoose.Schema ({
+    companyName: {
+        type: String,
+        required: true
+    },
+    jobtitle: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    startdate: {
+        type: Date,
+        required: true
+    },
+    enddate: {
+        type: Date,
+        required: false // Slutdatum behövs inte om det är ett pågående jobb
+    }
+})
+
 // Routes
 app.get("/api", async (req, res) => {
     res.json({message: "Welcome to this API"});
